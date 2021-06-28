3602ec3ec98d5d13fd9fec4683fb9528912824d0
ainafp <ainafp@gmail.com>
2021-06-28 23:48:37 +0200

    figuring out icons

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,6 +8,7 @@ SITESUBTITLE=u'PhD'
 SITEURL = 'ainafp.github.io'
 PROFILE_IMAGE='aina_gscholar.jpeg'
 BIO='I am a research scientist with an interest in signal processing, machine learning and data analysis in healthcare, with experience in brain imaging research'
+LOAD_CONTENT_CACHE=False
 
 PATH = 'content'
 PAGE_PATHS = ['pages']
@@ -21,26 +22,17 @@ TIMEZONE = 'Europe/Paris'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-#CATEGORY_FEED_ATOM = None
-#TRANSLATION_FEED_ATOM = None
-#AUTHOR_FEED_ATOM = None
-#AUTHOR_FEED_RSS = None
-#FEED_ATOM = None
-#FEED_RSS = None
-#FEED_ALL_ATOM = None
-#FEED_ALL_RSS = None
-#TAG_FEED_ATOM = None
-
-# Blogroll
-#LINKS = (('Google scholar', 'https://scholar.google.fr/citations?user=ilC7VXwAAAAJ&hl=en&oi=sra'),
-#('LinkedIn','https://www.linkedin.com/in/aina-frau-pascual-3116a936?trk=nav_responsive_tab_profile_pic'),
-#('Github','https://github.com/ainafp'),
-#('Research Gate','https://www.researchgate.net/profile/Aina_Frau_Pascual'),)
+CATEGORY_FEED_ATOM=None
+TRANSLATION_FEED_ATOM=None
+AUTHOR_FEED_ATOM=None
+AUTHOR_FEED_RSS=None
+FEED_ATOM=None
+FEED_RSS=None
+FEED_ALL_ATOM=None
+FEED_ALL_RSS=None
+TAG_FEED_ATOM=None
 
 # Social widget
-#SOCIAL = None
-#SOCIAL = (('You can add links in your config file', '#'),
-#          ('Another social link', '#'),)
 SOCIAL=(
 ('google-scholar', 'https://scholar.google.fr/citations?user=ilC7VXwAAAAJ&hl=en&oi=sra'),
 ('linkedin','https://www.linkedin.com/in/aina-frau-pascual-3116a936?trk=nav_responsive_tab_profile_pic'),
